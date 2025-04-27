0f31306576a7b3be3303ff5275688c94ef6ada4f
Benjamin Minixhofer <bminixhofer@gmail.com>
2025-04-27 16:13:01 +0000

    source env

--- rust_utils/tpu_build.sh
+++ rust_utils/tpu_build.sh
@@ -1,6 +1,7 @@
 # otherwise use 'maturin develop --release'
 set -e
 
+. ../tokenkit_env/bin/activate
 python3 -m maturin build --release
 mv $PWD/target/wheels/rust_utils-0.14.1.dev0-cp310-cp310-manylinux_2_34_x86_64.whl $PWD/target/wheels/rust_utils-0.14.1.dev0-cp310-none-any.whl
 pip install --force-reinstall $PWD/target/wheels/rust_utils-0.14.1.dev0-cp310-none-any.whl
